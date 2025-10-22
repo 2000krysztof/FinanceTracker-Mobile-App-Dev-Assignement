d856c5bbd02df17bc1354c79d1e77fc5d5f3bd0a
2000krysztof <2000krzysztof@gmail.com>
2025-10-22 15:35:33 +0100

    Refactor nav bar to a function

--- app/src/main/java/com/example/finamcetracker/MainActivity.kt
+++ app/src/main/java/com/example/finamcetracker/MainActivity.kt
@@ -12,23 +12,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        handleNavbar()
 
+
+    }
+
+
+    fun handleNavbar(){
         val navBar = findViewById<BottomNavigationView>(R.id.navBar)
 
         navBar.setOnItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.nav_home ->{
-
+                    Log.d("testing clicks", "home")
                     true
                 }
 
                 R.id.nav_budget_list ->{
-
+                    Log.d("testing clicks", "list")
                     true
                 }
 
                 R.id.nav_settings ->{
-
+                    Log.d("testing clicks", "settings")
                     true
                 }
                 else->false;
